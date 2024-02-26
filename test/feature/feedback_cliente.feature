c56082c21eb93e950edfeb8079f29bf5aba90075
KevinCndor <kevin.condor@epn.edu.ec>
2024-02-26 07:21:11 -0500

    Cambio de orden de escenarios

--- test/feature/feedback_cliente.feature
+++ test/feature/feedback_cliente.feature
@@ -4,31 +4,6 @@ Característica: Recolección de feedback de compras de los clientes (servicio/p
   quiero que mis clientes puedan calificar el producto y el servicio después del proceso de compra de un producto
   para aprender de los resultados y mejorar la satisfacción de mis clientes.
 
-  Escenario: Obtener feedback de las calificaciones de los clientes sobre el servicio
-    Dado que el Cliente ha dado su feedback sobre el producto
-    Y se tiene un Servicio con las siguientes valoraciones totales
-      | total_de_calificaciones | cantidad_de_estrellas | porcentaje_de_calificaciones |
-      | 10                      | 1                     | 30%                          |
-      | 6                       | 2                     | 18%                          |
-      | 2                       | 3                     | 6%                           |
-      | 5                       | 4                     | 15%                          |
-      | 10                      | 5                     | 30%                          |
-    Cuando el Cliente envíe una Calificación de tres sobre cinco estrellas del Servicio
-    Y seleccione algunas de las siguientes causas de su Calificación para el Servicio
-      | causas           |
-      | Paquete dañado   |
-      | Entrega tardía   |
-      | Entrega a tiempo |
-      | Entrega rápida   |
-    Entonces la valoración total de calificaciones de 3 estrellas del Servicio aumentará en 1 de la siguiente manera
-    Y el vendedor podrá visualizar el siguiente reporte con todas las causas en orden descendente
-      | cantidad_de_estrellas | porcentaje_de_calificaciones | causas             |
-      | 1                     | 29%                          |                    |
-      | 2                     | 18%                          | Entrega tardía (1) |
-      | 3                     | 9%                           | Paquete dañado (1) |
-      | 4                     | 15%                          |                    |
-      | 5                     | 29%                          |                    |
-
   Escenario: Obtener feedback de las calificaciones de los clientes sobre el producto
     Dado que el Cliente ha realizado el pago y el proceso de envío de la compra ha finalizado
     Y se tiene un Producto con las siguientes valoraciones totales
@@ -57,3 +32,28 @@ Característica: Recolección de feedback de compras de los clientes (servicio/p
       | 3                     | 40%                          | Mal funcionamiento (2)                               |
       | 4                     | 0%                           |                                                      |
       | 5                     | 40%                          | Buenos acabados (2), Buena calidad de materiales (2) |
+
+  Escenario: Obtener feedback de las calificaciones de los clientes sobre el servicio
+    Dado que el Cliente ha dado su feedback sobre el producto
+    Y se tiene un Servicio con las siguientes valoraciones totales
+      | total_de_calificaciones | cantidad_de_estrellas | porcentaje_de_calificaciones |
+      | 10                      | 1                     | 30%                          |
+      | 6                       | 2                     | 18%                          |
+      | 2                       | 3                     | 6%                           |
+      | 5                       | 4                     | 15%                          |
+      | 10                      | 5                     | 30%                          |
+    Cuando el Cliente envíe una Calificación de tres sobre cinco estrellas del Servicio
+    Y seleccione algunas de las siguientes causas de su Calificación para el Servicio
+      | causas           |
+      | Paquete dañado   |
+      | Entrega tardía   |
+      | Entrega a tiempo |
+      | Entrega rápida   |
+    Entonces la valoración total de calificaciones de 3 estrellas del Servicio aumentará en 1 de la siguiente manera
+    Y el vendedor podrá visualizar el siguiente reporte con todas las causas en orden descendente
+      | cantidad_de_estrellas | porcentaje_de_calificaciones | causas             |
+      | 1                     | 29%                          |                    |
+      | 2                     | 18%                          | Entrega tardía (1) |
+      | 3                     | 9%                           | Paquete dañado (1) |
+      | 4                     | 15%                          |                    |
+      | 5                     | 29%                          |                    |
--- test/feature/steps/feedback_cliente.py
+++ test/feature/steps/feedback_cliente.py
@@ -4,93 +4,6 @@ from modelo.ModeloFeedback import *
 use_step_matcher("re")
 
 
-# SERVICIO
-@step("que el Cliente ha dado su feedback sobre el producto")
-def step_impl(context):
-    context.producto = Producto(1, "Martillo", "De madera")
-    arreglo_productos_pedidos = [context.producto]
-    context.pedido = Pedido(1, "Entregado", 6, "direccion", arreglo_productos_pedidos)
-
-    assert context.producto.feedback_producto_esta_dado()
-
-
-@step("se tiene un Servicio con las siguientes valoraciones totales")
-def step_impl(context):
-    calificaciones_totalizadas = 0
-    lista_porcentajes_por_estrella = list()
-
-    for i in context.pedido.servicio.puntuaciones_calificaciones:
-        calificaciones_totalizadas += i["cantidad"]
-
-    for row in context.table:
-        total_de_calificaciones = int(row["total_de_calificaciones"])
-        cantidad_de_estrellas = int(row["cantidad_de_estrellas"])
-        porcentaje_de_calificaciones = row["porcentaje_de_calificaciones"]
-        lista_porcentajes_por_estrella.append(porcentaje_de_calificaciones)
-
-        assert context.pedido.servicio.puntuaciones_calificaciones[cantidad_de_estrellas - 1][
-                   "cantidad"] == total_de_calificaciones, "No se tiene el total de calificaciones correcto"
-
-    context.pedido.servicio.calcular_porcentajes()
-    for i in context.pedido.servicio.puntuaciones_calificaciones:
-        assert i["porcentaje"] == lista_porcentajes_por_estrella[int(i["estrellas"]) - 1], ("No se tiene el porcentaje "
-                                                                                            "de calificaciones correcto")
-
-
-@step("el Cliente envíe una Calificación de tres sobre cinco estrellas del Servicio")
-def step_impl(context):
-    context.cliente = Cliente("1752458974", "Juan", "Herrera", "juan.herrera@hotmail.com", "0984759642",
-                              context.pedido)
-    context.cliente.calificar_servicio(context.pedido, 3, ["Paquete dañado"], context.producto)
-
-    assert context.pedido.servicio.puntuaciones_calificaciones[2]["cantidad"] == 3, "No se ha calificado correctamente el producto"
-    return True
-
-@step("seleccione algunas de las siguientes causas de su Calificación para el Servicio")
-def step_impl(context):
-    causas = list()
-    causa_seleccionada = ""
-
-    for row in context.table:
-        causas.append(row["causas"])
-
-    for causa in causas:
-        if causa == "Paquete dañado":
-            causa_seleccionada = causa
-
-    if context.pedido.servicio.calificaciones_recibidas[-1] is None:
-        for causa_buscada in context.pedido.servicio.calificaciones_recibidas[-1].causas:
-            if causa_buscada == causa_seleccionada:
-                assert True, "No se ha seleccionado la causa correctamente"
-
-@step("la valoración total de calificaciones de 3 estrellas del Servicio aumentará en 1 de la siguiente manera")
-def step_impl(context):
-    assert context.pedido.servicio.puntuaciones_calificaciones[2]["cantidad"] == 3, "No se ha aumentado la calificación correctamente"
-
-
-@step("el vendedor podrá visualizar el siguiente reporte con todas las causas en orden descendente")
-def step_impl(context):
-    lista_porcentajes_por_estrella = list()
-    lista_causas_esperadas = list()
-    lista_causas_obtenidas = list()
-
-    lista_porcentajes_por_estrella = context.pedido.servicio.obtener_porcentajes_de_calificaciones()
-
-    for row in context.table:
-        cantidad_de_estrellas = int(row["cantidad_de_estrellas"])
-        porcentaje_de_calificaciones = row["porcentaje_de_calificaciones"]
-        causas = row["causas"]
-        lista_causas_esperadas.append(causas)
-        assert lista_porcentajes_por_estrella[
-                   cantidad_de_estrellas - 1] == porcentaje_de_calificaciones, ("No se tiene el porcentaje de "
-                                                                                "calificaciones correcto")
-
-    for i in range(1, 6, 1):
-        lista_causas_obtenidas.append(context.pedido.servicio.obtener_causas_de_cada_estrella()[i])
-        print(lista_causas_obtenidas[i - 1] + " == " + lista_causas_esperadas[i - 1])
-        assert lista_causas_obtenidas[i - 1] == lista_causas_esperadas[i - 1], "No se tienen las causas correcta"
-
-
 @step("que el Cliente ha realizado el pago y el proceso de envío de la compra ha finalizado")
 def step_impl(context):
     context.producto = Producto(1, "Martillo", "De madera")
@@ -149,6 +62,7 @@ def step_impl(context):
                 assert True, "No se ha seleccionado la causa correctamente"
 
 
+# SERVICIO
 @step("la valoración total de calificaciones de 3 estrellas del Producto aumentará en 1")
 def step_impl(context):
     assert context.producto.calificaciones[3] == 2, "No se ha aumentado la calificación correctamente"
@@ -175,3 +89,89 @@ def step_impl(context):
         lista_causas_obtenidas.append(context.producto.obtener_causas_de_cada_estrella()[i])
         print(lista_causas_obtenidas[i - 1] + " == " + lista_causas_esperadas[i - 1])
         assert lista_causas_obtenidas[i - 1] == lista_causas_esperadas[i - 1], "No se tienen las causas correcta"
+
+        @step("que el Cliente ha dado su feedback sobre el producto")
+        def step_impl(context):
+            context.producto = Producto(1, "Martillo", "De madera")
+            arreglo_productos_pedidos = [context.producto]
+            context.pedido = Pedido(1, "Entregado", 6, "direccion", arreglo_productos_pedidos)
+
+            assert context.producto.feedback_producto_esta_dado()
+
+        @step("se tiene un Servicio con las siguientes valoraciones totales")
+        def step_impl(context):
+            calificaciones_totalizadas = 0
+            lista_porcentajes_por_estrella = list()
+
+            for i in context.pedido.servicio.puntuaciones_calificaciones:
+                calificaciones_totalizadas += i["cantidad"]
+
+            for row in context.table:
+                total_de_calificaciones = int(row["total_de_calificaciones"])
+                cantidad_de_estrellas = int(row["cantidad_de_estrellas"])
+                porcentaje_de_calificaciones = row["porcentaje_de_calificaciones"]
+                lista_porcentajes_por_estrella.append(porcentaje_de_calificaciones)
+
+                assert context.pedido.servicio.puntuaciones_calificaciones[cantidad_de_estrellas - 1][
+                           "cantidad"] == total_de_calificaciones, "No se tiene el total de calificaciones correcto"
+
+            context.pedido.servicio.calcular_porcentajes()
+            for i in context.pedido.servicio.puntuaciones_calificaciones:
+                assert i["porcentaje"] == lista_porcentajes_por_estrella[int(i["estrellas"]) - 1], (
+                    "No se tiene el porcentaje "
+                    "de calificaciones correcto")
+
+        @step("el Cliente envíe una Calificación de tres sobre cinco estrellas del Servicio")
+        def step_impl(context):
+            context.cliente = Cliente("1752458974", "Juan", "Herrera", "juan.herrera@hotmail.com", "0984759642",
+                                      context.pedido)
+            context.cliente.calificar_servicio(context.pedido, 3, ["Paquete dañado"], context.producto)
+
+            assert context.pedido.servicio.puntuaciones_calificaciones[2][
+                       "cantidad"] == 3, "No se ha calificado correctamente el producto"
+            return True
+
+        @step("seleccione algunas de las siguientes causas de su Calificación para el Servicio")
+        def step_impl(context):
+            causas = list()
+            causa_seleccionada = ""
+
+            for row in context.table:
+                causas.append(row["causas"])
+
+            for causa in causas:
+                if causa == "Paquete dañado":
+                    causa_seleccionada = causa
+
+            if context.pedido.servicio.calificaciones_recibidas[-1] is None:
+                for causa_buscada in context.pedido.servicio.calificaciones_recibidas[-1].causas:
+                    if causa_buscada == causa_seleccionada:
+                        assert True, "No se ha seleccionado la causa correctamente"
+
+        @step("la valoración total de calificaciones de 3 estrellas del Servicio aumentará en 1 de la siguiente manera")
+        def step_impl(context):
+            assert context.pedido.servicio.puntuaciones_calificaciones[2][
+                       "cantidad"] == 3, "No se ha aumentado la calificación correctamente"
+
+        @step("el vendedor podrá visualizar el siguiente reporte con todas las causas en orden descendente")
+        def step_impl(context):
+            lista_porcentajes_por_estrella = list()
+            lista_causas_esperadas = list()
+            lista_causas_obtenidas = list()
+
+            lista_porcentajes_por_estrella = context.pedido.servicio.obtener_porcentajes_de_calificaciones()
+
+            for row in context.table:
+                cantidad_de_estrellas = int(row["cantidad_de_estrellas"])
+                porcentaje_de_calificaciones = row["porcentaje_de_calificaciones"]
+                causas = row["causas"]
+                lista_causas_esperadas.append(causas)
+                assert lista_porcentajes_por_estrella[
+                           cantidad_de_estrellas - 1] == porcentaje_de_calificaciones, ("No se tiene el porcentaje de "
+                                                                                        "calificaciones correcto")
+
+            for i in range(1, 6, 1):
+                lista_causas_obtenidas.append(context.pedido.servicio.obtener_causas_de_cada_estrella()[i])
+                print(lista_causas_obtenidas[i - 1] + " == " + lista_causas_esperadas[i - 1])
+                assert lista_causas_obtenidas[i - 1] == lista_causas_esperadas[
+                    i - 1], "No se tienen las causas correcta"
