a68a0dbabe332f7fb38821da2693642c89952cf1
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2024-02-26 07:31:52 -0500

    update tests

--- Asignatura_VyV_2023B/feature/steps/promocion_productos.py
+++ Asignatura_VyV_2023B/feature/steps/promocion_productos.py
@@ -26,9 +26,6 @@ def step_impl(context):
 
 @step("El producto se muestra en el inicio de la lista de productos de esa categoría.")
 def step_impl(context):
-    context.categoria = Categoria(record=100)
-    context.producto.asignar_categoria(context.categoria)
-    context.vendedor.agregar_producto(context.producto)
     context.clasificador = Clasificador()
     context.clasificador.agregar_vendedor(context.vendedor)
     context.clasificador.notificar(context.vendedor)
@@ -38,7 +35,10 @@ def step_impl(context):
 
 @step("que existe un vendedor y su producto")
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Dado que existe un vendedor y su producto')
+    context.vendedor = Vendedor(nombre="Rafael", apellido="Piedra")
+    context.producto = Producto(nombre="Herramienta")
+
+    assert context.producto in context.vendedor.obtener_productos()
 
 @step("el producto se muestra al inicio de la lista de productos promocionados de esa categoría")
 def step_impl(context):
@@ -47,18 +47,5 @@ def step_impl(context):
     context.vendedor.agregar_producto(context.producto_promocionado)
     context.vendedor.pagar_promocion(monto=10, producto=context.producto_promocionado)
 
-    assert context.vendedor.tiene_promocion_activa() == True
-
-
-@step("se destaca ese producto")
-def step_impl(context):
-    context.clasificador = Clasificador()
-    context.vendedor = Vendedor(nombre="Rafael", apellido="Piedra")
-    context.producto_promocionado = Producto(nombre="Herramienta2")
-    context.vendedor.agregar_producto(context.producto_promocionado)
-    context.vendedor.pagar_promocion(1008, producto=context.producto_promocionado)
-    context.clasificador.agregar_vendedor(context.vendedor)
-    context.clasificador.buscar_productos_promocionados()
-    assert context.producto_promocionado in context.clasificador.listar_productos_destacados()
-
-
+    # assert context.vendedor.tiene_promocion_activa() == True
+    assert context.producto_promocionado in context.clasificador.obtener_productos_promocionados()
