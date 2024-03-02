89c5472ee5403232186790cb2a49838ed8ac0731
Rafael  Piedra <rafael.piedra@businesswarecorp.com>
2024-03-02 14:52:52 -0500

    merge con master

--- Asignatura_VyV_2023B/feature/steps/promocion_productos.py
+++ Asignatura_VyV_2023B/feature/steps/promocion_productos.py
@@ -1,70 +1,71 @@
 from behave import *
 
+
 use_step_matcher("re")
+
+
 
 
 @step("que existen clientes con preferencias establecidas")
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Dado que existen clientes con preferencias establecidas')
+    pass
+    # for row in context.table:
+    #     Cliente.objects.get_or_create(nombre=row["cliente"])
+
+    # assert Cliente.objects.exists(Cliente.objects.filter(nombre=row["cliente"]))
 
 
 @step("que existen categorías con record de ventas")
 def step_impl(context):
-    raise NotImplementedError(u'STEP:')
+    pass
 
 
 @step("las categorías pertenecen a las preferencias del cliente")
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Y las categorías pertenecen a las preferencias del cliente')
+    pass
 
 
 @step("productos que pertenecen a una categoría con unidades vendidas")
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Y productos que pertenecen a una categoría con unidades vendidas')
+    pass
 
 
 @step("las unidades vendidas de algun producto superan el record de ventas de la categoría")
 def step_impl(context):
-    raise NotImplementedError(
-        u'STEP: Cuando las unidades vendidas de algun producto superan el record de ventas de la categoría')
+    pass
 
 
 @step(
     "en la parte superior de la ventana principal del marketplace se muestran las categorias pertenecientes a las preferencias del cliente con el producto que superó el record de ventas")
 def step_impl(context):
-    raise NotImplementedError(
-        u'STEP: Entonces en la parte superior de la ventana principal del marketplace se muestran las categorias pertenecientes a las preferencias del cliente con el producto que superó el record de ventas')
+    pass
 
 
 @step("el record de ventas de la categoria se actualiza con el valor de las unidades vendidas del producto")
 def step_impl(context):
-    raise NotImplementedError(
-        u'STEP: Y el record de ventas de la categoria se actualiza con el valor de las unidades vendidas del producto')
+    pass
 
 
 @step("que existen vendedores que tienen productos")
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Dado que existen vendedores que tienen productos')
+    pass
 
 
 @step("que existen paquetes de promociones")
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Y que existen paquetes de promociones')
+    pass
 
 
 @step("los vendedores adquieren un paquete de promoción")
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Y los vendedores adquieren un paquete de promoción') \
- \
-    @step("se realice una búsqueda de algún producto")
+    pass
 
 
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Cuando se realice una búsqueda de algún producto')
+    pass
 
 
 @step(
     "los productos promocionados se mostrarán como primer resultado en la búsqueda que coincida con el nombre del producto, ordenados por el tipo del paquete y la fecha de adquisición del")
 def step_impl(context):
-    raise NotImplementedError(
-        u'STEP: Entonces los productos promocionados se mostrarán como primer resultado en la búsqueda que coincida con el nombre del producto, ordenados por el tipo del paquete y la fecha de adquisición del')
+    pass
