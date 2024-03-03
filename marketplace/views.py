15ae9957191837983a5eb6b71bad0809951a0dc8
Rafael  Piedra <rafael.piedra@businesswarecorp.com>
2024-03-03 15:01:53 -0500

    udpate cambios

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,19 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from marketplace.models import Producto
-
-
-# from marketplace.models import Producto
-
-
-# Create your views here.
+from marketplace.models import Producto, Categoria
 
 
 def index(request):
-    return render(request, 'home.html', context={"productos_destacados": []})
+    return render(request, 'home.html',
+                  context={"products": Producto.objects.all(), "categorias": Categoria.objects.all()})
 
 
 def buscar_producto(request):
     query = request.GET.get('q', '')
     productos = Producto.objects.filter(nombre__icontains=query).order_by('-promocion')
-    return render(request, 'plantilla_hija_ejemplo.html', {'productos': productos, 'query': query})
+    return render(request, 'home.html', {'productos': productos, 'query': query})
